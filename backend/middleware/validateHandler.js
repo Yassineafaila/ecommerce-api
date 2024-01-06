32a15792216fdd37f2ec087452f8f3a8d072bcbe
Yassineafaila <yassineafaila55@gmail.com>
2024-01-06 19:36:39 +0000

    feat:validate the image input

--- backend/middleware/validateHandler.js
+++ backend/middleware/validateHandler.js
@@ -1,47 +1,56 @@
-const { body, validationResult } = require('express-validator')
+const { body, validationResult, check } = require("express-validator");
 const userLoginValidationRules = () => {
-  console.log("im right now validtaing")
+  console.log("im right now validtaing");
   return [
-    
     //email must be full
-    body('email',"email is empty").not().isEmpty(),
+    body("email", "email is empty").not().isEmpty(),
     // the email  must be an email
-    body("email","it's not a valide email").isEmail(),
+    body("email", "it's not a valide email").isEmail(),
     // password must be at least 8 chars long
-    body('password',"password must be at least 8 characters").isLength({ min: 6 }),
-  ]
-}
-const userRegisterValidationRules=()=>{
-    return[
-        // name must not be empty
-        body("name","name is empty").not().isEmpty(),
-        // email must not be empty
-        body("email","email is empty").not().isEmpty(),
-        //email must be an valide email
-        body("email","must be a valid email").isEmail(),
-        // password must be at least 8 chars long
-        body("password","password must be at least 8 characters").isLength({min:8})
-    ]
-}
-
+    body("password", "password must be at least 8 characters").isLength({
+      min: 6,
+    }),
+  ];
+};
+const userRegisterValidationRules = () => {
+  return [
+    // name must not be empty
+    body("name", "name is empty").not().isEmpty(),
+    // email must not be empty
+    body("email", "email is empty").not().isEmpty(),
+    //email must be an valide email
+    body("email", "must be a valid email").isEmail(),
+    // password must be at least 8 chars long
+    body("password", "password must be at least 8 characters").isLength({
+      min: 8,
+    }),
+  ];
+};
+const validateProductDetails = () => {
+  return [
+    // image must not be empty
+    check("image").notEmpty().withMessage("You must select an image"),
+  ];
+};
 
 const validate = (req, res, next) => {
-  const errors = validationResult(req)
-  console.log(errors)
-  console.log('error happened')
+  const errors = validationResult(req);
+  console.log(errors);
+  console.log("error happened");
   if (errors.isEmpty()) {
-    return next()
+    return next();
   }
-  const extractedErrors = []
-  errors.array().map(err => extractedErrors.push({ [err.path]: err.msg }))
-  console.log(extractedErrors)
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
+  console.log(extractedErrors);
   return res.status(422).json({
     errors: extractedErrors,
-  })
-}
+  });
+};
 
 module.exports = {
-    userLoginValidationRules,
-    userRegisterValidationRules,
+  userLoginValidationRules,
+  userRegisterValidationRules,
+  validateProductDetails,
   validate,
-}
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,11 +10,12 @@ const {
 } = require("../controllers/productController");
 const { validateToken, validateAdmin } = require("../middleware/authHandler");
 const uploadFilesMiddleware = require("../middleware/uploadImagesHandler");
+const { validateProductDetails, validate } = require("../middleware/validateHandler");
 const productRouter = express.Router();
 productRouter
   .route("/")
   .get(getProducts)
-    .post(validateToken, validateAdmin,uploadFilesMiddleware,createProduct);
+    .post(validateToken, validateAdmin,validateProductDetails(),validate,uploadFilesMiddleware,createProduct);
 productRouter.route("/my-products").get(validateToken,validateAdmin,getAllProducts)
 productRouter
   .route("/:id")
